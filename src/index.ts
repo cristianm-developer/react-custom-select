47850039cf676b2b20ff2a087f65b7421206220b
Criss <criss@faso.com.br>
2025-08-04 20:53:02 -0300

    exported props types

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ import { Prefix, Suffix } from "./components/Addon.component";
 import Select from "./components/Select.component";
 import Option from "./components/Option.component";
 
+import type { OptionProps, OptionObjectRef } from "./components/Option.component";
+import type { SelectProps, SelectObjectRef } from "./components/Select.component";
+
+
 export {Select, Prefix, Suffix, Option}
+export type { SelectObjectRef, OptionObjectRef, OptionProps, SelectProps}
